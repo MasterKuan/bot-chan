257d5f830036ce383438a58ea2772163cbc22d7f
Ryan Kuan <ryanlingkuan@gmail.com>
2020-06-16 03:44:56 -0700

    Increased page search. Checks 50 times on a page for a post that is a jpg, png, or gif.

--- index.js
+++ index.js
@@ -10,44 +10,82 @@ client.once("ready", () => {
 })
 
 client.on("message", message => {
-    var content = "";
-
-    if(message.content.startsWith(`${prefix}animemes`)) {
+    if(message.content.startsWith(`${prefix}animemes`))
+    {
         getPost("animemes")
-            .then(content => message.channel.send(content));
+            .then(content => message.channel.send(content.title, {files: [content.url]}))
+            .catch(error => console.log(error));
     }
-    else if(message.content.startsWith(`${prefix}animeirl`)) {
+    else if(message.content.startsWith(`${prefix}animeirl`))
+    {
         getPost("anime_irl")
-            .then(content => message.channel.send(content));
+            .then(content => message.channel.send(content.title, {files: [content.url]}))
+            .catch(error => console.log(error));
     }
-    else if(message.content.startsWith(`${prefix}hmemes`)) {
+    else if(message.content.startsWith(`${prefix}hanimemes`))
+    {
         getPost("hentaimemes")
-            .then(content => message.channel.send(content));
+            .then(content => message.channel.send(content.title, {files: [content.url]}))
+            .catch(error => console.log(error));
     }
-
+    else if(message.content.startsWith(`${prefix}moescape`))
+    {
+        getPost("moescape")
+            .then(content => message.channel.send(content.title, {files: [content.url]}))
+            .catch(error => console.log(error));
+    }
+    else if(message.content.startsWith(`${prefix}fuckinweeb`))
+    {
+        getPost("whataweeb")
+            .then(content => message.channel.send(content.title, {files: [content.url]}))
+            .catch(error => console.log(error));
+    }
+    else if(message.content.startsWith(`${prefix}test`))
+    {
+        content => message.channel.send("Test", {files: ["https://i.imgur.com/YGVw2po.gif"]})
+    }
 })
 
 // Gets a random post from a random page between 1 and 5
-function getPost(subreddit) {
+function getPost(subreddit)
+{
     var url = "https://www.reddit.com/r/" + subreddit + ".json?limit=100&after="
-    var page = Math.floor(Math.random() * 5);
+    var page = Math.floor(Math.random() * 10);
     var after = "";
     var content = "";
 
     // Go to random page
-    for(var i = 0; i < page; i++) {
+    for(var i = 0; i < page; i++)
+    {
         after = fetch(url + after)
             .then(res => res.json())
             .then(json => { return json.data.after; });
     }
 
-    var content = fetch(url + after)
+    var post = fetch(url + after)
         .then(res => res.json())
-        .then(json => json.data.children.map(v => v.data.url))
-        .then(posts => {
-            var randomURL = posts[Math.floor(Math.random() * posts.length)];
-            return randomURL;
+        .then(json => json.data.children.map(v => v.data))
+        .then(posts_list => {
+            var counter = 0;
+            // Looks for an image/gif that it can post
+            do
+            {
+                var num = Math.floor(Math.random() * posts_list.length);
+                var randomPost = posts_list[num];
+
+                counter++;
+                if(counter >= 50)
+                {
+                    randomPost = {"title":"404 Couldn't find something to post", "url":"https://i.imgur.com/YGVw2po.gif"}
+                    break;
+                }
+                console.log(randomPost.url);
+            }while (!randomPost.url.endsWith(".gif") &&
+                    !randomPost.url.endsWith(".png") &&
+                    !randomPost.url.endsWith(".jpg"));
+
+            return randomPost;
         });
 
-    return content;
+    return post;
 }
